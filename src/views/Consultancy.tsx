0e582deab3f69db692b6f5085731767b4151f860
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2025-05-27 11:39:32 -0600

    Completed legal defense and consultancy

--- src/views/Consultancy.tsx
+++ src/views/Consultancy.tsx
@@ -1,7 +1,21 @@
+import { ArrowContainer } from "../components"
+
 export const Consultancy = () => {
     return (
-        <p>Imparto cursos de capacitación que te permiten mantenerte actualizado y adquirir conocimientos clave en materia aduanera y de comercio exterior.
-
-            Los cursos están diseñados para agregar valor a tus operaciones y ayudarte a manejar cualquier situación compleja que surja en el entorno aduanero.</p>
+        <section className="mt-2 flex flex-row items-center text-white">
+            <section className="w-1/2 text-justify">
+                <ArrowContainer text="Capacitación y actualización en materia aduanera y de comercio exterior." />
+                <div>Imparto cursos de capacitación que te permiten mantenerte actualizado y adquirir conocimientos clave en materia aduanera y de comercio exterior.
+                </div>
+                <br />
+                <div>Los cursos están diseñados para agregar valor a tus operaciones y ayudarte a manejar cualquier situación compleja que surja en el entorno aduanero.</div>
+                <div className="bg-gray-900 p-2 text-center">
+                    <a href="https://wa.me/5212294639014" target="_blank" className="p-5 text-center">¿Te interesa capacitarte? Conoce más sobre nuestros cursos</a>
+                </div>
+            </section>
+            <section className="w-1/2">
+                <img src="/Consultancy.webp" alt="consultoría" />
+            </section>
+        </section>
     )
 }
--- src/views/LegalDefense.tsx
+++ src/views/LegalDefense.tsx
@@ -1,10 +1,9 @@
+import { ArrowContainer } from '../components/ArrowContainer';
 export const LegalDefense = () => {
     return (
         <section className="flex flex-row text-white">
-            <section>
-                <div>
-                    Defensa legal ante actos y resoluciones definitivas de autoridad aduanera.
-                </div>
+            <section className='w-1/2'>
+                <ArrowContainer text='Defensa legal ante actos y resoluciones definitivas de autoridad aduanera.' />
                 <div>Estudiamos los actos de autoridad que puedan afectar tus intereses y definimos una estrategia legal robusta para conseguir el mejor resultado posible. ofrecemos:</div>
                 <ul className="list-disc text-left">
                     <li>Recursos de Revocación para impugnar actos de autoridad aduanera definitivos y determinantes de créditos fiscales.</li>
@@ -12,11 +11,10 @@ export const LegalDefense = () => {
                     <li>Juicios de amparo que protegen tus derechos humanos ante actos de las autoridades aduaneras.</li>
                 </ul>
                 <div className="bg-gray-900 p-2">
-                    <a href="https://wa.me/5212294639014" target="_blank" className="p-5 text-center">¿Buscas defensa legal especializada? Consulta cómo
-                        podemos ayudarte</a>
+                    <a href="https://wa.me/5212294639014" target="_blank" className="p-5 text-center">¿Buscas defensa legal especializada? Consulta cómo podemos ayudarte</a>
                 </div>
             </section>
-            <section>
+            <section className='w-1/2'>
                 <img src="../src/assets/img/Management.webp" alt="gestión" className="w-80" />
             </section>
         </section>
