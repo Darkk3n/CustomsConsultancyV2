98df60b45cf6c7701fe8288a3c870d84e9256949
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2025-05-27 09:59:26 -0600

    Fixed arrow container

--- src/components/ArrowContainer.tsx
+++ src/components/ArrowContainer.tsx
@@ -5,7 +5,8 @@ interface IArrowContainerProps {
 
 export const ArrowContainer = ({ text }: IArrowContainerProps) => {
     return (
-        <div className="bg-blue-900 text-white p-6 clip-arrow max-w-xl">
+        <div className="flex flex-row text-white p-6 clip-arrow max-w-xl" style={{ backgroundColor: '#001129' }}>
+            <img src="/arrow.svg" alt="arrow" className="w-10 h-10 mr-4" />
             <h2 className="text-xl font-bold">{text}</h2>
         </div>
     )
