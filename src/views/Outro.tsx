4d40b949cfeee2510e57c8619d35d4767e3bd4e9
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2025-05-27 15:52:10 -0600

    Added outro content

--- src/views/Outro.tsx
+++ src/views/Outro.tsx
@@ -1,5 +1,14 @@
 export const Outro = () => {
     return (
-        <div>Outro</div>
+        <section className="flex flex-row mt-3">
+            <section className="w-1/2">
+                <img src="/Outro.webp" alt="outro" className="w-full" />
+            </section>
+            <section className="w-1/2">
+                <p className="text-white text-2xl text-justify ml-3">Mi trayectoria incluye haber formado parte de la Administración Local Jurídica de Veracruz, así como Abogado de Aduanas Marítimas del Servicio de Administración Tributaria, así como en el sector privado como asesor jurídico de agentes aduanales, empresas y particulares, proporcionado soluciones prácticas y legales adaptadas a las necesidades de mis clientes, proporcionando tranquilidad y certeza en sus operaciones de comercio exterior.</p>
+                <br />
+                <p className="text-white text-2xl text-justify ml-3">Si deseas más información o necesitas apoyo en un caso específico, estoy para ayudarte. Escríbeme y platicamos sobre cómo puedo contribuir a solucionar tus necesidades.</p>
+            </section>
+        </section>
     )
 }
