8c18c6a32e1d8440bf1f41cab496969ec93faebd
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2025-12-18 23:42:53 -0600

    Fixed Management component

--- src/views/Management.tsx
+++ src/views/Management.tsx
@@ -1,14 +1,17 @@
-import { ArrowContainer, SectionContainer } from "../components"
+import { ArrowContainer } from '../components'
+import { SectionContainer } from '../components/SectionContainer'
 
 export const Management = () => {
     return (
-        <SectionContainer className="text-white">
+        <SectionContainer split className="text-white">
             <section className="w-full md:w-1/2 space-y-4 text-sm sm:text-base md:text-xl">
                 <ArrowContainer text="Gestión y resolución de procedimientos administrativos aduaneros" />
+
                 <p>
                     Te acompaño en la gestión y resolución de tus procedimientos
                     administrativos aduaneros, incluyendo:
                 </p>
+
                 <ul className="list-disc list-inside space-y-2 text-left">
                     <li>Depósito ante la Aduana de Mercancías.</li>
                     <li>Recuperación de mercancías que causaron abandono.</li>
@@ -29,14 +32,14 @@ export const Management = () => {
                     paso del proceso.
                 </p>
             </section>
-            <section className="w-full md:w-1/2 flex flex-col items-center space-y-4">
+            <section className="w-full md:w-1/2 flex flex-col items-center gap-4">
                 <img
                     src="/Management.webp"
                     alt="gestión"
                     className="w-full max-w-xs sm:max-w-sm md:max-w-md h-auto object-contain rounded"
                 />
 
-                <div className="bg-gray-900 w-full text-center rounded">
+                <div className="bg-gray-900 rounded text-center w-full max-w-md">
                     <a
                         href="https://wa.me/5212294639014"
                         target="_blank"
