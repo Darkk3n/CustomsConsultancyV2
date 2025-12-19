324f78eb4bbdb4879ff2e6c10af3b61d75f987e9
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2025-12-18 22:19:41 -0600

    Mobile adaptation Outro

--- src/views/Outro.tsx
+++ src/views/Outro.tsx
@@ -1,29 +1,62 @@
-import { faFacebook, faInstagram, faLinkedin, faTiktok, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
-import { SocialMediaIcon } from "../components/SocialMediaIcon"
+import {
+    faFacebook,
+    faInstagram,
+    faLinkedin,
+    faTiktok,
+    faWhatsapp,
+} from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope, faMobileScreen } from '@fortawesome/free-solid-svg-icons'
 import { ProfileTitle } from '../components'
+import { SocialMediaIcon } from '../components/SocialMediaIcon'
 
 export const Outro = () => {
     return (
-        <section className="flex flex-row mt-3">
-            <section className="w-1/2">
-                <img src="/Outro.webp" alt="outro" className="w-full" />
+        <section className="mt-10 flex flex-col md:flex-row gap-8 px-4 md:px-8">
+            {/* Image */}
+            <section className="w-full md:w-1/2 flex justify-center">
+                <img
+                    src="/Outro.webp"
+                    alt="outro"
+                    className="w-full max-w-sm sm:max-w-md md:max-w-lg h-auto object-contain rounded"
+                />
             </section>
-            <section className="w-1/2">
+
+            {/* Content */}
+            <section className="w-full md:w-1/2 space-y-5 text-white">
                 <ProfileTitle />
-                <p className="text-white text-2xl text-justify ml-3">Mi trayectoria incluye haber formado parte de la Administración Local Jurídica de Veracruz, así como Abogado de Aduanas Marítimas del Servicio de Administración Tributaria, así como en el sector privado como asesor jurídico de agentes aduanales, empresas y particulares, proporcionado soluciones prácticas y legales adaptadas a las necesidades de mis clientes, proporcionando tranquilidad y certeza en sus operaciones de comercio exterior.</p>
-                <br />
-                <p className="text-white text-2xl text-justify ml-3">Si deseas más información o necesitas apoyo en un caso específico, estoy para ayudarte. Escríbeme y platicamos sobre cómo puedo contribuir a solucionar tus necesidades.</p>
-                <div className='flex flex-row items-center justify-around mt-5 ml-6'>
-                    <SocialMediaIcon icon={faMobileScreen} text='229-463-9014' />
-                    <SocialMediaIcon icon={faEnvelope} text='juridico.aduanero.ags@gmail.com' isBold />
+
+                <p className="text-sm sm:text-base md:text-xl text-justify">
+                    Mi trayectoria incluye haber formado parte de la Administración Local
+                    Jurídica de Veracruz, así como Abogado de Aduanas Marítimas del Servicio
+                    de Administración Tributaria, además de desempeñarme en el sector
+                    privado como asesor jurídico de agentes aduanales, empresas y
+                    particulares, proporcionando soluciones prácticas y legales adaptadas
+                    a las necesidades de mis clientes.
+                </p>
+
+                <p className="text-sm sm:text-base md:text-xl text-justify">
+                    Si deseas más información o necesitas apoyo en un caso específico,
+                    estoy para ayudarte. Escríbeme y platicamos sobre cómo puedo contribuir
+                    a solucionar tus necesidades.
+                </p>
+
+                {/* Contact info */}
+                <div className="flex flex-col sm:flex-row gap-4 justify-between">
+                    <SocialMediaIcon icon={faMobileScreen} text="229-463-9014" />
+                    <SocialMediaIcon
+                        icon={faEnvelope}
+                        text="juridico.aduanero.ags@gmail.com"
+                        isBold
+                    />
                 </div>
-                <div className='flex flex-row items-center justify-around mt-5'>
-                    <SocialMediaIcon icon={faWhatsapp} link='https://wa.me/5212294639014' />
-                    <SocialMediaIcon icon={faFacebook} link='https://www.facebook.com/ic.aduanal.5' />
-                    <SocialMediaIcon icon={faTiktok} link='https://www.tiktok.com/@juridicoaduaneroags?_t=8rVP0nXmsQa&_r=1' />
-                    <SocialMediaIcon icon={faInstagram} link='https://www.instagram.com/juridicoags?igsh=aDdqbXQ1bWk1Ynlo' />
-                    <SocialMediaIcon icon={faLinkedin} link='https://www.linkedin.com/in/andr%C3%A9s-aguilar-s%C3%A1nchez-a78967273?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app' />
+
+                {/* Social icons */}
+                <div className="flex flex-wrap justify-center sm:justify-between gap-4 pt-2">
+                    <SocialMediaIcon icon={faWhatsapp} link="https://wa.me/5212294639014" />
+                    <SocialMediaIcon icon={faFacebook} link="https://www.facebook.com/ic.aduanal.5" />
+                    <SocialMediaIcon icon={faTiktok} link="https://www.tiktok.com/@juridicoaduaneroags" />
+                    <SocialMediaIcon icon={faInstagram} link="https://www.instagram.com/juridicoags" />
+                    <SocialMediaIcon icon={faLinkedin} link="https://www.linkedin.com/in/andr%C3%A9s-aguilar-s%C3%A1nchez-a78967273" />
                 </div>
             </section>
         </section>
