cfea2a875e8de8af44fc0dcf07a4f02efdc849ab
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2025-12-18 22:06:26 -0600

    Micro change to ProfileTitle

--- src/components/ProfileTitle.tsx
+++ src/components/ProfileTitle.tsx
@@ -1,7 +1,9 @@
 export const ProfileTitle = () => {
     return (
-        <div className="bg-gray-900 rounded-lg p-5">
-            <span className="profile-title text-4xl text-white">Mtro. Andres Aguilar Sanchez</span>
+        <div className="bg-gray-900 rounded-lg p-4 sm:p-5 text-center">
+            <span className="profile-title text-2xl sm:text-3xl md:text-4xl text-white font-bold">
+                Mtro. Andrés Aguilar Sanchez
+            </span>
         </div>
     )
 }
