320f8fd3bcbdd797ab82cc57b6e5fddbddc8c3a0
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2025-12-20 01:13:19 -0600

    Inverted content

--- src/components/SectionContainer.tsx
+++ src/components/SectionContainer.tsx
@@ -5,6 +5,7 @@ interface SectionProps {
    children: ReactNode
    split?: boolean
    centered?: boolean
+   reverse?: boolean
    className?: string
 }
 
@@ -12,13 +13,16 @@ export const SectionContainer = ({
    children,
    split = false,
    centered = false,
+   reverse = false,
    className,
 }: SectionProps) => {
    return (
       <section
          className={clsx(
             'px-4 md:px-8 mt-10',
-            split ? 'flex flex-col md:flex-row gap-8' : 'flex flex-col gap-6',
+            split && 'flex flex-col md:flex-row gap-8',
+            split && reverse && 'md:flex-row-reverse',
+            !split && 'flex flex-col gap-6',
             centered && 'items-center text-center',
             className
          )}
