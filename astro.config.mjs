1b57a6b9e6ef207da41b3b4a608d06d94a258297
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2026-04-10 16:52:41 -0600

    Implemented outro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import tailwindv4 from '@tailwindcss/vite'; // Import the Vite plugin
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+
 export default defineConfig({
-   vite: {
-      plugins: [tailwindv4()],
-   },
+  vite: {
+     plugins: [tailwindv4()],
+  },
+
+  integrations: [icon()],
 });
